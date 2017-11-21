304e54f8ed201300e0401b39c153af806b3afc35
Reginaldo <Reginaldo@Reginaldo-PC>
2017-11-21 18:51:13 -0300

    controller da compra/ salvando os primeiros dados da compra no banco

--- tutor/imports/import_header.php
+++ tutor/imports/import_header.php
@@ -1,5 +1,5 @@
 <header>
     <i class="fa fa-bars btn-active-menu"></i>
-    <h3 class="title">SisWebAgenda</h3>
+    <h3 class="title">SCA 1.0</h3>
     <img src="./images/Logo_UNICATÓLICA_Branco.png" alt="" class="" width="216" height="50">
 </header>
