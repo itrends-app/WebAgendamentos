505b39c7a7b5a0317989a75f43903b957d4ce9ef
Reginaldo <Reginaldo@Reginaldo-PC>
2017-12-06 08:18:06 -0300

    funções do caixa adicionadas

--- aluno/php/envia_email.php
+++ aluno/php/envia_email.php
@@ -6,15 +6,16 @@ include_once './pega_ultimo_agendamento.php';
 $email = $_POST["email"];//pega email do aluno
 $monitor_id = $_SESSION['monitor_selecionado'];
 
-$header = "<div style='width:100%;padding:20px 0;color:#fff;font-size:20px;background-color:#232323;text-align:center;'>WebAgendamentos</div>";
-
+$divOpen = "<div style='background-color: silver;width:500px;margin:0 auto;'>";
+$header = "<div style='font-size:20px;text-align:center;'>SCA 1.0</div>";
+$divClose = "</div>";
 
 //Mensagem que o sistema irá enviar para o aluno da disciplina
-$mensagem = $header."<h3>Olá, você confirmou a sua presença no encontro de monitoria. <br>Categoria: " . utf8_encode($_SESSION['nome_categoria']) . ""
+$mensagem = $divOpen.$header."<h3>Olá, você confirmou a sua presença no encontro de monitoria. <br>Categoria: " . utf8_encode($_SESSION['nome_categoria']) . ""
         . "<br>Curso: " . utf8_encode($_SESSION['disciplina']) . "<br>Monitor: " . $_SESSION['monitor_selecionado2'] . "<br>Agendado para: " . $_SESSION['dia_marcado'] . " de "
         . "" . $_SESSION['mes_marcado'] . " de " . $_SESSION['ano_marcado'] . " " . $_SESSION['hora_marcada'] . "<br><br><br>"
         . "<a href='http://neadteste.fcrs.edu.br/uniagendamentos/aluno/php/pega_dados_via_email.php?id=$lastid&email=$email'>Clique aqui para cancelar o agendamento</a><br>"
-        . "<a href='http://neadteste.fcrs.edu.br/uniagendamentos/aluno/php/pega_dados_via_email.php?id=$lastid&email=$email&edt=$monitor_id'>Clique aqui para editar o agendamento</a></h3>";
+        . "<a href='http://neadteste.fcrs.edu.br/uniagendamentos/aluno/php/pega_dados_via_email.php?id=$lastid&email=$email&edt=$monitor_id'>Clique aqui para editar o agendamento</a></h3>".$divClose;
 $mensagem_full = utf8_decode($mensagem);
 
 //Mensagem que o sistema irá enviar para o tutor da disciplina
